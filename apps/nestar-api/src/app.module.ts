d366851d3fa7d1d30c0cee032ab37d73aae9ebfa
Ziynatilloh <ziynatillokht@gmail.com>
2025-03-02 16:56:50 +0900

    feat: built project standarts

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -5,11 +5,15 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
+import { ComponentsModule } from './components/components.module';
+import { DatabseModule } from './databse/databse.module';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot(),
 		GraphQLModule.forRoot({ driver: ApolloDriver, playground: true, uploads: false, autoSchemaFile: true }),
+		ComponentsModule,
+		DatabseModule,
 	],
 	controllers: [AppController],
 	providers: [AppService, AppResolver],
