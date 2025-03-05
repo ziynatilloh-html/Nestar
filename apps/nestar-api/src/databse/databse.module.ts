1a11a942b3c2e77cd7804374bed47787b44852a1
Ziynatilloh <ziynatillokht@gmail.com>
2025-03-05 15:13:48 +0900

    fix: connect MongoDB databese

--- apps/nestar-api/src/databse/databse.module.ts
+++ apps/nestar-api/src/databse/databse.module.ts
@@ -1,4 +1,22 @@
 import { Module } from '@nestjs/common';
-
-@Module({})
-export class DatabseModule {}
+import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+@Module({
+	imports: [
+		MongooseModule.forRootAsync({
+			useFactory: () => ({
+				uri: process.env.NODE_ENV === 'production' ? process.env.MONGO_PROD : process.env.MONGO_DEV,
+			}),
+		}),
+	],
+	exports: [MongooseModule],
+})
+export class DatabseModule {
+	constructor(@InjectConnection() private readonly connection: Connection) {
+		if (connection.readyState === 1) {
+			console.log(`MongoDB connected into ${process.env.NODE_ENV === 'production' ? 'production' : 'development'} DB`);
+		} else {
+			console.log('MongoDb connection failed');
+		}
+	}
+}
