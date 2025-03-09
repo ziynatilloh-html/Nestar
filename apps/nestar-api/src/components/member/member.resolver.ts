9f99f8b10916bba25260aea25d99679e3167eac6
Ziynatilloh <ziynatillokht@gmail.com>
2025-03-09 15:35:00 +0900

    feat: develop Login business logic

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -20,13 +20,13 @@ export class MemberResolver {
 			throw new InternalServerErrorException(err);
 		}
 	}
-	@Mutation(() => String)
+	@Mutation(() => Member)
 	@UsePipes(ValidationPipe)
-	public async login(@Args('input') input: LoginInput): Promise<string> {
+	public async login(@Args('input') input: LoginInput): Promise<Member> {
 		try {
 			console.log('Mutation: login');
 			console.log('input', input);
-			return this.memberService.login();
+			return this.memberService.login(input);
 		} catch (err) {
 			console.log('Error, Login', err);
 			throw new InternalServerErrorException(err);
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,8 +1,10 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Member } from '../../libs/dto/member/member';
-import { MemberInput } from '../../libs/dto/member/member.input';
+import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { MemberStatus } from '../../libs/enums/member.enum';
+import { Message } from '../../libs/enums/common.enum';
 
 @Injectable()
 export class MemberService {
@@ -20,8 +22,18 @@ export class MemberService {
 		}
 	}
 
-	public async login(): Promise<string> {
-		return 'login executed';
+	public async login(input: LoginInput): Promise<Member> {
+		const { memberNick, memberPassword } = input;
+		const response = await this.memberModel.findOne({ memberNick: memberNick }).select('+memberPassword').exec();
+		if (!response || response.memberStatus === MemberStatus.DELETE) {
+			throw new InternalServerErrorException(Message.NO_MEMBER_NICK);
+		} else if (response.memberStatus === MemberStatus.BLOCK) {
+			throw new InternalServerErrorException(Message.BLOCKED_USER);
+		}
+		//TODO compare passwords
+		const isMatch = memberPassword === response.memberPassword;
+		if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
+		return response;
 	}
 	public async updateMember(): Promise<string> {
 		return 'updateMember executed';
