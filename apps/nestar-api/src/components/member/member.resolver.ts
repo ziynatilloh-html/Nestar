931e4da1c9661c9e6a724fa5452da0bfa4bb589b
Ziynatilloh <ziynatillokht@gmail.com>
2025-03-09 14:06:35 +0900

    feat: integrate validation Pipes logic

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,18 +1,24 @@
-import { Query, Mutation, Resolver } from '@nestjs/graphql';
+import { Query, Mutation, Resolver, Args } from '@nestjs/graphql';
 import { MemberService } from './member.service';
+import { UsePipes, ValidationPipe } from '@nestjs/common';
+import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 
 @Resolver()
 export class MemberResolver {
 	constructor(private readonly memberService: MemberService) {}
 
 	@Mutation(() => String)
-	public async signup(): Promise<string> {
+	@UsePipes(ValidationPipe)
+	public async signup(@Args('input') input: MemberInput): Promise<string> {
 		console.log('Mutation: signup');
+		console.log('input', input);
 		return this.memberService.signup();
 	}
 	@Mutation(() => String)
-	public async login(): Promise<string> {
+	@UsePipes(ValidationPipe)
+	public async login(@Args('input') input: LoginInput): Promise<string> {
 		console.log('Mutation: login');
+		console.log('input', input);
 		return this.memberService.login();
 	}
 	@Mutation(() => String)
