1698e055f121a01da80eed37ad22637e58087f18
Ziynatilloh <ziynatillokht@gmail.com>
2025-04-07 07:18:07 +0900

    fix: modify job scheduler business logic

--- apps/nestar-batch/src/batch.module.ts
+++ apps/nestar-batch/src/batch.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
 import { BatchController } from './batch.controller';
-
-import { ConfigModule } from '@nestjs/config';
-
 import { BatchService } from './batch.service';
-import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import PropertySchema from 'apps/nestar-api/src/schemas/Property.model';
+import MemberSchema from 'apps/nestar-api/src/schemas/Member.model';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-	imports: [ConfigModule.forRoot(), DatabaseModule, ScheduleModule.forRoot()],
+	imports: [
+		ConfigModule.forRoot(),
+		DatabaseModule,
+		ScheduleModule.forRoot(),
+		MongooseModule.forFeature([{ name: 'Property', schema: PropertySchema }]),
+		MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]),
+	],
 	controllers: [BatchController],
 	providers: [BatchService],
 })
